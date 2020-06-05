25e8945531380b9e0180274e9fd172bc9e9eb799
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-06-05 13:03:54 +0200

    Fin du projet et rendu final.

--- Application/Client/Client/Views/AccountCreation.Designer.cs
+++ Application/Client/Client/Views/AccountCreation.Designer.cs
@@ -333,6 +333,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
             this.Name = "frmAccountCreation";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MessagingApp - Account creation";
